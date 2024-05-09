3865767492467658567bc9ac8f4df8f2e11c032f
le2yj <le2yj94@gmail.com>
2024-05-09 23:57:41 +0900

    :sparkles: 배열 선언

---
+++ src/w02/Arrays.java
@@ -0,0 +1,24 @@
+package w02;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        // 배열 선언
+        // 일반적인 선언방식
+        /*
+        int[] intArray;    // 정수 배열
+        long[] longArray;
+        double[] doubleArray;  // 실수 배열
+        char[] charArray;  // 문자 배열
+        String[] stringArray; // 문자열 배열
+        */
+
+        // 2번째 선언방식
+        int intArray[];    // 정수 배열
+        long longArray[];
+        double doubleArray[];  // 실수 배열
+        char charArray[];  // 문자 배열
+        String stringArray[]; // 문자열 배열
+
+    }
+}

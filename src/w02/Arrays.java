5ae0a2f51317381a66041b26b7e6a64d55d908d8
le2yj <le2yj94@gmail.com>
2024-05-10 00:02:34 +0900

    :heavy_plus_sign: 배열 순회

--- src/w02/Arrays.java
+++ src/w02/Arrays.java
@@ -14,11 +14,20 @@ public class Arrays {
         */
 
         // 2번째 선언방식
-        int intArray[];    // 정수 배열
+//        int intArray[];    // 정수 배열
         long longArray[];
         double doubleArray[];  // 실수 배열
         char charArray[];  // 문자 배열
         String stringArray[]; // 문자열 배열
+
+
+        //길이가 8인 정수배열 선언
+        int[] intArray = new int[3];
+
+        // 배열이 초기화된 값을 출력해본다.
+        for (int i = 0; i < intArray.length; i++) { // .length 는 배열의 길이
+            System.out.println(intArray[i]);
+        }
 
     }
 }

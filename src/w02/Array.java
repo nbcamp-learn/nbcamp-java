90de5ff30a165590655e14bacfa617e8fa360503
le2yj <le2yj94@gmail.com>
2024-05-11 23:50:35 +0900

    :heavy_plus_sign: 얕은 복사
    Signed-off-by: le2yj <le2yj94@gmail.com>

--- src/w02/Array.java
+++ src/w02/Array.java
@@ -56,5 +56,14 @@ public class Array {
         for (int i : intArray) {
             System.out.print(i);  // 11111
         }
+
+        // 얕은 복사
+        int[] a = {1, 2, 3, 4};
+        int[] b = a; // 얕은 복사
+
+        b[0] = 3; // b 배열의 0번째 순번값을 3으로 수정했습니다. (1 -> 3)
+
+        System.out.println(a[0]); // 출력 3  <- a 배열의 0번째 순번값도 3으로 조회됩니다.
+
     }
 }

6821eedc09c48b52dd885024637fed40b1c5bdac
le2yj <le2yj94@gmail.com>
2024-05-13 23:57:20 +0900

    :heavy_plus_sign: 깊은 복사 메서드
    Signed-off-by: le2yj <le2yj94@gmail.com>

--- src/w02/Array.java
+++ src/w02/Array.java
@@ -67,6 +67,7 @@ public class Array {
         System.out.println(a[0]); // 출력 3  <- a 배열의 0번째 순번값도 3으로 조회됩니다.
         */
 
+        /*
         // 깊은 복사
         int[] a = {1, 2, 3, 4};
         int[] b = new int[a.length];
@@ -78,6 +79,17 @@ public class Array {
         b[0] = 3; // b 배열의 0번째 순번값을 3으로 수정했습니다. (1 -> 3)
 
         System.out.println(a[0]); // 출력 1 <- 깊은 복사를 했기때문에 a 배열은 그대로 입니다.
+         */
+
+        // 깊은 복사 메서드
+        // 1. clone() 메서드
+        int[] a = {1, 2, 3, 4};
+        int[] b = a.clone(); // 가장 간단한 방법입니다.
+        // 하지만, clone() 메서드는 2차원이상 배열에서는 얕은 복사로 동작합니다!!
+
+        // 2. Arrays.copyOf() 메서드
+        int[] c = {1, 2, 3, 4};
+        int[] d = Arrays.copyOf(c, c.length); // 배열과 함께 length값도 같이 넣어줍니다.
 
     }
 }

2707c0080fe7659347f042365ab502ccea4a8621
Eevee <le2yj94@gmail.com>
2024-06-30 20:34:21 +0900

    :memo: .

--- src/W03/Main.java
+++ src/W03/Main.java
@@ -2,6 +2,7 @@ package W03;
 
 public class Main {
     public static void main(String[] args) {
+
         Car[] carArray = new Car[3];
         Car car1 = new Car();
         car1.changeGear('P');
